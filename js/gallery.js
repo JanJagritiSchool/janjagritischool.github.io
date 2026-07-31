6c2b1f299d744ebfd5cc08e8b3c304075fbfa00e
JJSS <adhikariminprasad988@gmail.com>
2026-08-01 00:03:27 +0545

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,73 @@
+/*========================================
+JANA JAGRITI SECONDARY SCHOOL
+Gallery Lightbox JavaScript
+gallery.js
+========================================*/
 
+
+const galleryImages = document.querySelectorAll(".gallery-img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImg = document.getElementById("lightbox-img");
+
+const closeBtn = document.querySelector(".close");
+
+
+
+galleryImages.forEach(image => {
+
+
+    image.addEventListener("click", function(){
+
+
+        lightbox.style.display = "flex";
+
+
+        lightboxImg.src = this.src;
+
+
+    });
+
+
+});
+
+
+
+
+if(closeBtn){
+
+
+    closeBtn.addEventListener("click",()=>{
+
+
+        lightbox.style.display = "none";
+
+
+    });
+
+
+}
+
+
+
+
+if(lightbox){
+
+
+    lightbox.addEventListener("click",(e)=>{
+
+
+        if(e.target !== lightboxImg){
+
+
+            lightbox.style.display = "none";
+
+
+        }
+
+
+    });
+
+
+}
