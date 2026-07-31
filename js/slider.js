e6776843f3cc9e370e1d08f075aa5bcb10a9f5a1
JJSS <adhikariminprasad988@gmail.com>
2026-07-31 22:40:59 +0545

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,50 @@
+/*========================================
+JANA JAGRITI SECONDARY SCHOOL
+Hero Background Slider
+slider.js
+========================================*/
 
+
+const slides = document.querySelectorAll(".hero-slide");
+
+
+let currentSlide = 0;
+
+
+
+function changeSlide(){
+
+
+    if(slides.length === 0){
+
+        return;
+
+    }
+
+
+
+    slides[currentSlide].classList.remove("active");
+
+
+
+    currentSlide++;
+
+
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+
+
+    slides[currentSlide].classList.add("active");
+
+
+}
+
+
+
+
+setInterval(changeSlide,5000);
